4b9525046831169f1cae786c72eb779858dbc0c3
GuardianLux <unholyagent@gmail.com>
2020-06-02 11:05:13 +1000

    fixed typo in config

--- tag-store/src/index.js
+++ tag-store/src/index.js
@@ -24,7 +24,7 @@ Amplify.configure({
   API: {
     endpoints: [
       {
-        name: "notes",
+        name: "tags",
         endpoint: config.apiGateway.URL,
         region: config.apiGateway.REGION
       },
