f33dd09fc9a1511c8e6af910e3954a8d598d1c77
Sid Raj <siddharth.rajesh13@gmail.com>
2020-06-08 12:33:55 +1000

    login integration with cognito partial

--- tag-store/src/containers/sign-in.js
+++ tag-store/src/containers/sign-in.js
@@ -1,23 +1,51 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography, Box } from '@material-ui/core';
 import { flexbox, positions, spacing } from '@material-ui/system';
 import { Button } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
+import { Auth } from 'aws-amplify';
+import { useAppContext } from '../libs/contextLib'
 
 export default function SignIn() {
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+
+    const { userHasAuthenticated } = useAppContext()    // use hook to update state and pass to context
+
+    async function handleSignIn(e) {
+        e.preventDefault()
+        try {
+            await Auth.signIn(userName, password)
+            alert("Logged In")
+            userHasAuthenticated(true)
+        } catch (err) {
+            alert(err.message)
+        }
+    }
+
     return (
-            <Box flexGrow={1} display="flex" alignItems="center" flexDirection="column" top="40%" left="25%" right="25%" position="absolute" mx="auto">
-                <Typography variant="h2">Login</Typography>
-                <TextField id="filled-basic" label="Username" variant="filled" />
-                <TextField
-                    id="filled-password-input"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    variant="filled"
-                />
-                <Button variant="contained" href="/">Sign In</Button>
-            </Box>
+        <Box flexGrow={1} display="flex" alignItems="center" flexDirection="column" top="40%" left="25%" right="25%" position="absolute" mx="auto">
+            <Typography variant="h2">Login</Typography>
+            <TextField
+                id="filled-basic"
+                label="Username"
+                variant="filled"
+                onChange={e => setUserName(e.target.value)}
+            />
+            <TextField
+                id="filled-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="filled"
+                onChange={e => setPassword(e.target.value)}
+            />
+            <Button
+            variant="contained"
+            href="/"
+            onClick={handleSignIn}
+            >Sign In</Button>
+        </Box>
     );
 }
--- tag-store/src/index.js
+++ tag-store/src/index.js
@@ -5,6 +5,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import SignIn from './containers/sign-in';
+import { Amplify } from 'aws-amplify'
+import config from './config'
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  }
+})
 
 ReactDOM.render(
   <Router>
