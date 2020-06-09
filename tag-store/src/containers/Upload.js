83e9b4b4ac6eeab303aea0ab6ff12ed704f6ade7
GuardianLux <unholyagent@gmail.com>
2020-06-09 18:07:17 +1000

    image upload progress, need to fix 403 error

--- tag-store/src/containers/Upload.js
+++ tag-store/src/containers/Upload.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Typography, Box, Button, TextField } from '@material-ui/core';
 import { flexbox, positions, spacing } from '@material-ui/system';
 import { s3Upload } from "../libs/awsLib";
+import config from "../config";
 import { useHistory } from 'react-router-dom'
 
 export default function Upload() {
-  const upload = (e) => {
-    s3Upload(e.target.files[0])
+  const file = useRef(null);
+  const upload = (file) => {
+    s3Upload(file)
+  }
+
+  function handleFileChange(event) {
+    file.current = event.target.files[0];
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+  
+    try {
+      upload(file);
+      history.push("/Navigation");
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   const history = useHistory()
@@ -16,9 +33,10 @@ export default function Upload() {
   }
 
   return (
-    <Box>
+    <Box display="flex" alignItems="center" flexDirection="column" top="40%" left="25%" right="25%" position="absolute" mx="auto">
       <Typography variant="h1">Upload an Image</Typography>
-      <input type="file" onChange={upload} />
+      <input type="file" onChange={handleFileChange} />
+      <input type="button" value="Upload" onClick={handleSubmit} />
       <input type="button" value="Back" onClick={handleBackClick} />
     </Box>
   );
