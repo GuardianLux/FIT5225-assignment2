116c0ecad6eee839cfd113f322a7c8d00ca3a696
GuardianLux <unholyagent@gmail.com>
2020-06-10 17:49:08 +1000

    user input now becomes array ready for api

--- tag-store/src/containers/Query.js
+++ tag-store/src/containers/Query.js
@@ -4,17 +4,24 @@ import { flexbox, positions, spacing } from '@material-ui/system';
 import { useHistory } from 'react-router-dom'
 
 export default function Query() {
-
     const history = useHistory()
 
     function handleBackClick(e) {
       history.push("/Navigation")
     }
 
+    function handleTextChange(e) {
+        var tagArray = e.target.value.split(",")
+        if (tagArray.length > 10) {
+            alert("Only 10 tags allowed")
+        }
+        return tagArray
+    }
+
     return (
         <Box display="flex" alignItems="center" flexDirection="column" top="40%" left="25%" right="25%" position="absolute" mx="auto">
         <Typography variant="h1">Run Query</Typography>
-        <TextareaAutosize aria-label="minimum height" rowsMin={3} placeholder="Enter tags seperated by ','" />
+        <TextareaAutosize aria-label="minimum height" rowsMin={3} placeholder="Enter tags seperated by ','" id="tags" onChange={handleTextChange.bind(this)}/>
         <Button variant="contained">Execute</Button>
         <Button variant="contained" onClick={handleBackClick}>Back</Button>
       </Box>
