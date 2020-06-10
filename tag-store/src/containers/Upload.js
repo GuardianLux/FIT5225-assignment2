2e30fba4b968630596b7b49a7890f8945b52dd7f
GuardianLux <unholyagent@gmail.com>
2020-06-10 13:48:21 +1000

    Image uploading complete

--- tag-store/src/containers/Upload.js
+++ tag-store/src/containers/Upload.js
@@ -1,30 +1,34 @@
 import React, { useState, useRef } from 'react';
 import { Typography, Box, Button, TextField } from '@material-ui/core';
 import { flexbox, positions, spacing } from '@material-ui/system';
-import { s3Upload } from "../libs/awsLib";
+import { Storage } from "aws-amplify";
 import config from "../config";
 import { useHistory } from 'react-router-dom'
 
 export default function Upload() {
-  const file = useRef(null);
-  const upload = (file) => {
-    s3Upload(file)
-  }
 
-  function handleFileChange(event) {
-    file.current = event.target.files[0];
-  }
+ var fileUrl = ''
+ var filename = ''
+ var file = ''
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-  
-    try {
-      upload(file);
-      history.push("/Navigation");
-    } catch (e) {
-      console.log(e);
-    }
-  }
+ function handleChange(e){
+   file = e.target.files[0]
+   fileUrl = URL.createObjectURL(file)
+   filename = file.name
+ }
+
+ function save(){
+   Storage.put(filename, file)
+    .then(() => {
+      console.log('Upload success')
+      fileUrl = ''
+      filename = ''
+      file=''
+    })
+    .catch(err => {
+      console.log(err)
+    })
+ }
 
   const history = useHistory()
 
@@ -35,8 +39,9 @@ export default function Upload() {
   return (
     <Box display="flex" alignItems="center" flexDirection="column" top="40%" left="25%" right="25%" position="absolute" mx="auto">
       <Typography variant="h1">Upload an Image</Typography>
-      <input type="file" onChange={handleFileChange} />
-      <input type="button" value="Upload" onClick={handleSubmit} />
+      <input type="file" onChange={handleChange} />
+      <img src={fileUrl} />
+      <input type="button" value="Upload" onClick={save} />
       <input type="button" value="Back" onClick={handleBackClick} />
     </Box>
   );
